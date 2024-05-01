0acd8e3b55fcba2ae1765a968ae358c8540bacc5
Vee B <64688451+veneciab@users.noreply.github.com>
2024-05-02 06:49:14 +1000

    Annoying text assignment
    Display annoying text by utilising upper case function, f strings

---
+++ Lesson 2_Variables_Strings/annoying_text.py
@@ -0,0 +1,12 @@
+original_text = "The mountains are calling "
+shouting_text = original_text.upper()
+print(shouting_text)
+
+annoying_text = 10*(shouting_text)
+print(annoying_text)
+
+print(f"My annoying text is {annoying_text}\n")
+print(f"It has {len(annoying_text)} characters.")
+
+
+
